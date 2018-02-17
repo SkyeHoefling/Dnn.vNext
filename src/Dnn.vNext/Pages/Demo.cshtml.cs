c17e1189a641586320ae1ac8d48af1e3a68661b5
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-02-17 00:55:37 -0500

    Added modules folder which contains a Simple Form; Updated demo page which automatically renders partial into our drag and drop control

--- src/Dnn.vNext/Pages/Demo.cshtml.cs
+++ src/Dnn.vNext/Pages/Demo.cshtml.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Dnn.vNext.Pages
 {
     public class DemoModel : PageModel
     {
+        public string ModulePath { get; } = "Modules/SimpleForm";
+
         public void OnGet()
         {
 
