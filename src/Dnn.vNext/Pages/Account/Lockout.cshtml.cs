fd4d8a16511834b12ee40c724fca0495a700fab1
Andrew Hoefling <andrew@hoeflingsoftware.com>
2018-02-16 20:16:40 -0500

    Initial commit

---
+++ src/Dnn.vNext/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dnn.vNext.Pages.Account
+{
+    public class LockoutModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
